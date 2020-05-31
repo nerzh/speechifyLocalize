8325a9eda1b43ea7be95e488882dc2d274f8e479
Oleh Hudeichuk <emptystamp@gmail.com>
2020-05-31 17:06:06 +0300

    remove default subcommand

--- Sources/speechifyLocalize/main.swift
+++ Sources/speechifyLocalize/main.swift
@@ -8,7 +8,7 @@ struct LocalizableString: ParsableCommand {
         abstract: "A utility for parse localizable strings and convert to csv",
         version: "1.0.0",
         subcommands: [Parser.self, Converter.self, Validator.self],
-        defaultSubcommand: Parser.self
+        defaultSubcommand: nil
     )
 }
 
